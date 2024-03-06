ece739aa4606a990f53ddb8fc52e23fbe43cd35d
sanket-sapate <sanketsapatevnit@gmail.com>
2024-03-06 19:10:54 +0530

    fix display chat for /

--- frontend/src/Chatting/DisplayChat.jsx
+++ frontend/src/Chatting/DisplayChat.jsx
@@ -1,9 +1,12 @@
 import {useSelector} from 'react-redux/es/hooks/useSelector'
-export default function(){
-    const chat = useSelector(store=> store.pipe)
+export default function({location}){
+    const chat = useSelector(store=> store.chat)
+    const pipe = useSelector(store=> store.pipe)
     return <>
         {
-            chat?.map((item,index)=>{
+           location==='/'? chat?.map((item,index)=>{
+                return <div key={index}>{item}</div>
+            }): pipe?.map((item,index)=>{
                 return <div key={index}>{item}</div>
             })
         }
